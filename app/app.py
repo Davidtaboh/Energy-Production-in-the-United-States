322de0f7cacef6e24d9e31ef1b42e255f4c784c2
Luke <snekulekul@gmail.com>
2020-06-08 19:26:56 -0400

    push before presentation

--- app/app.py
+++ app/app.py
@@ -16,12 +16,6 @@ def comparisons():
 
     return render_template('comparisons.html')
 
-# @app.route('/data')
-# def data():
-#     # path='Energy_merged.csv'
-#     # energy_data = pd.read_csv(path)
-#     return jsonify(energy_data)    
-
 @app.route('/visualization1')
 def visualization1():
 
@@ -47,83 +41,15 @@ def visualization5():
 
     return render_template('visualization5.html')
 
+@app.route('/visualization6')
+def visualization6():
 
-    # Green & Conventional Energy Total 1990-2018 By State // Bargraph
-#     green_energy = session.query(energyData.state_id, energyData.energy_type, func.sum(energyData.generation_megawatthours)).\
-#         filter(energyData.energy_type == 'Green Energy').group_by(energyData.state_id, energyData.energy_type).order_by(energyData.state_id).all()
+    return render_template('visualization6.html')
 
-#     green_energy_total = []
-    
-#     for state_id, energy_type, generation_megawatthours in green_energy:
-#         energy_dict ={}
-#         energy_dict["State"] = state_id
-#         energy_dict['Energy Type'] = energy_type
-#         energy_dict['Energy Prodcued'] = generation_megawatthours
-        
-#         green_energy_total.append(energy_dict)
-        
-#     conv_energy = session.query(energyData.state_id, energyData.energy_type, func.sum(energyData.generation_megawatthours)).\
-#         filter(energyData.energy_type == 'Conventional').group_by(energyData.state_id, energyData.energy_type).order_by(energyData.state_id).all()
+@app.route('/visualization7')
+def visualization7():
 
-#     conv_energy_total = []
-    
-#     for state_id, energy_type, generation_megawatthours in conv_energy:
-#         energy_dict ={}
-#         energy_dict["State"] = state_id
-#         energy_dict['Energy Type'] = energy_type
-#         energy_dict['Energy Prodcued'] = generation_megawatthours
-
-#         conv_energy_total.append(energy_dict)
-    
-    
-#     # Green Vs Conventional Energy of Per Year For Country // Linegraph
-
-#     green_country_year = session.query(energyData.year_energy, func.sum(energyData.generation_megawatthours), energyData.energy_type).\
-#         filter(energyData.energy_type == 'Green Energy').group_by(energyData.year_energy,energyData.energy_type).\
-#             order_by(energyData.year_energy)
-    
-#     green_total_country = []
-#     for year_energy, energy_type, generation_megawatthours in green_country_year:
-#         energy_dict = {}
-#         energy_dict["Year"] = year_energy
-#         energy_dict["Energy Type"] = energy_type
-#         energy_dict['Energy Produced'] = generation_megawatthours
-        
-#         green_total_country.append(energy_dict)
-
-    
-#     conv_country_year = session.query(energyData.year_energy, func.sum(energyData.generation_megawatthours), energyData.energy_type).\
-#         filter(energyData.energy_type == 'Conventional').group_by(energyData.year_energy,energyData.energy_type).\
-#             order_by(energyData.year_energy)
-    
-#     conv_country_total = []
-#     for year_energy, energy_type, generation_megawatthours in conv_country_year:
-#         energy_dict = {}
-#         energy_dict["Year"] = year_energy
-#         energy_dict["Energy Type"] = energy_type
-#         energy_dict['Energy Produced'] = generation_megawatthours
-
-#         conv_country_total.append(energy_dict)
-
-#     # MAP - Total Energy Production By State From 1990 2018/ This is For Leaflet Map
-
-#     map_energy = session.query(energyData.state_id, func.sum(energyData.generation_megawatthours)).\
-#         group_by(energyData.state_id).order_by(energyData.state_id)
-        
-#     map_state = []
-
-#     for state_id, generation_megawatthours in map_energy:
-#         energy_dict = {}
-#         energy_dict["State"] = state_id
-#         energy_dict["Energy Produced"] = generation_megawatthours
-
-#         map_state.append(energy_dict)
-
-    
-#     return jsonify({'Bargraph':{'Green Energy':green_energy_total,'Conventional Energy':conv_energy_total}, 
-#     'Linegraph':{'Country Green Energy':green_total_country,'Country Conventional Energy': conv_country_total}, 
-#     'Map':{'Total Energy By State':map_state}})
-    
+    return render_template('visualization7.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
